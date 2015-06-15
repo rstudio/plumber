4471404b0e17b8080af50e759c542bb8571c9bf2
trestletech <jeff.allen@trestletechnology.net>
2015-06-15 00:04:04 -0500

    Send proper binary-encoded images.

--- R/processor-image.R
+++ R/processor-image.R
@@ -10,16 +10,10 @@ RapierProcessor$new(
   function(val, req, res, data){
     dev.off()
 
-    if (!requireNamespace("base64enc", quietly = TRUE)) {
-      stop("The base64enc package is not available but is required in order to serialize images.",
-           call. = FALSE)
-    }
-
-    # read base64 file
     con <- file(data$file, "rb")
     img <- readBin(con, "raw", file.info(data$file)$size)
     close(con)
-    res$body <- httpuv::rawToBase64(img)
+    res$body <- img
     res$setHeader("Content-type", "image/jpeg")
     res
   }
@@ -35,16 +29,10 @@ RapierProcessor$new(
   function(val, req, res, data){
     dev.off()
 
-    if (!requireNamespace("base64enc", quietly = TRUE)) {
-      stop("The base64enc package is not available but is required in order to serialize images.",
-           call. = FALSE)
-    }
-
-    # read base64 file
     con <- file(data$file, "rb")
     img <- readBin(con, "raw", file.info(data$file)$size)
     close(con)
-    res$body <- httpuv::rawToBase64(img)
+    res$body <- img
     res$setHeader("Content-type", "image/png")
     res
   }
