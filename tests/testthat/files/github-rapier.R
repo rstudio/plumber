ae80d8e03cb8586982604c9d07d547d9736eb63b
trestletech <jeff.allen@trestletechnology.net>
2015-06-23 08:25:08 -0500

    Removed extraneous functions.

--- tests/testthat/files/github-rapier.R
+++ tests/testthat/files/github-rapier.R
@@ -14,45 +14,6 @@ function(){
   resp
 }
 
-list_to_string <- function(the_list) {
-  cnames <- names(the_list)
-  allvalues <- lapply(cnames, function(name) {
-    item <- the_list[[name]]
-    if (is.list(item)) {
-      if (is.null(names(item))) {
-        paste(name, "[[", seq_along(item), "]] = ", item, sep = "", collapse = "<br />")
-      } else {
-        paste(name, "$", names(item), " = ", item, sep = "", collapse = "<br />")
-      }
-    } else if (is.environment(item)){
-      paste(name, "ENV", sep=" = ")
-    } else if (is.character(item) || is.numeric(item)){
-      paste(name, as.character(item), sep=" = ")
-    }
-    else {
-      paste(name, "?", sep=" = ")
-    }
-  })
-  paste(allvalues, collapse = "<br />")
-}
-
-env_to_list <- function(env){
-  vars <- ls(envir=env)
-  res <- lapply(vars, function(var){
-    print(var)
-    val <- get(var, envir=env)
-    if (is.list(val)){
-      return(list_to_string(val))
-    } else if (is.environment(val)){
-      return ("<environment>")
-    } else{
-      return(val)
-    }
-  })
-  names(res) <- vars
-  list_to_string(res)
-}
-
 #' Give GitHub Webhook a way to alert us about new pushes to the repo
 #' https://developer.github.com/webhooks/
 #' @post /update
