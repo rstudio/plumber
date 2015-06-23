0d79e17e60c8b0f924093cf59552e87223e35224
trestletech <jeff.allen@trestletechnology.net>
2015-06-23 00:09:54 -0500

    Better debugging.

--- tests/testthat/files/github-rapier.R
+++ tests/testthat/files/github-rapier.R
@@ -23,8 +23,13 @@ list_to_string <- function(the_list) {
       } else {
         paste(name, "$", names(item), " = ", item, sep = "", collapse = "<br />")
       }
-    } else {
-      paste(name, item, sep=" = ")
+    } else if (is.environment(item)){
+      paste(name, "ENV", sep=" = ")
+    } else if (is.character(item) || is.numeric(item)){
+      paste(name, as.character(item), sep=" = ")
+    }
+    else {
+      paste(name, "?", sep=" = ")
     }
   })
   paste(allvalues, collapse = "<br />")
@@ -33,6 +38,7 @@ list_to_string <- function(the_list) {
 env_to_list <- function(env){
   vars <- ls(envir=env)
   res <- lapply(vars, function(var){
+    print(var)
     val <- get(var, envir=env)
     if (is.list(val)){
       return(list_to_string(val))
@@ -46,11 +52,12 @@ env_to_list <- function(env){
   list_to_string(res)
 }
 
-
 #' Give GitHub Webhook a way to alert us about new pushes to the repo
 #' https://developer.github.com/webhooks/
-#' @post /update
+#' @get /update
 function(req){
+  saveRDS(req, file="req.Rds")
   print(env_to_list(req))
+
   1
 }
