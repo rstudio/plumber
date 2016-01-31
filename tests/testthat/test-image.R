a203463a7ba9a384c049334fd071026d80070248
trestletech <jeff.allen@trestletechnology.net>
2016-01-31 15:09:52 -0800

    Expand image range downward.

--- tests/testthat/test-image.R
+++ tests/testthat/test-image.R
@@ -10,10 +10,10 @@ test_that("Images are properly rendered", {
   r <- plumber$new("files/image.R")
   resp <- r$serve(make_req("GET", "/png"), PlumberResponse$new())
 
-  expect_gt(length(resp$body), 13000) # This changes based on R ver/OS, may not be useful.
+  expect_gt(length(resp$body), 4000) # This changes based on R ver/OS, may not be useful.
   expect_lt(length(resp$body), 25000)
 
   resp <- r$serve(make_req("GET", "/jpeg"), PlumberResponse$new())
-  expect_gt(length(resp$body), 13000) # This changes based on R ver/OS, may not be useful.
+  expect_gt(length(resp$body), 4000) # This changes based on R ver/OS, may not be useful.
   expect_lt(length(resp$body), 25000)
 })
