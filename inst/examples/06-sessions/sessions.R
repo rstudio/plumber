bb87a88d1a71bcdbfcc1b05bd884d95d377d06dc
trestletech <jeff.allen@trestletechnology.net>
2016-02-15 15:43:29 -0600

    Add session example.

--- inst/examples/06-sessions/sessions.R
+++ inst/examples/06-sessions/sessions.R
@@ -7,3 +7,16 @@ function(req, res){
   res$setCookie("visitcounter", count+1)
   return(paste0("This is visit #", count))
 }
+
+#* Example using req$session. Requires adding "sessionCookie()" support to your router in order
+#* to work:
+#* `pr <- plumb("file.R"); pr$addGlobalProcessor(sessionCookie("secret", "cookieName")); pr$run()`
+#* @get /sessionCounter
+function(req, res){
+  count <- 0
+  if (!is.null(req$session$counter)){
+    count <- as.numeric(req$session$counter)
+  }
+  req$session$counter <- count + 1
+  return(paste0("This is visit #", count))
+}
