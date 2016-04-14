b04d66478c80af4b6bc8af6b4944bd78e10150a1
trestletech <jeff.allen@trestletechnology.net>
2016-04-13 23:16:28 -0500

    Open up bounds of image size.

--- tests/testthat/test-image.R
+++ tests/testthat/test-image.R
@@ -10,10 +10,8 @@ test_that("Images are properly rendered", {
   r <- plumber$new("files/image.R")
   resp <- r$serve(make_req("GET", "/png"), PlumberResponse$new())
 
-  expect_gt(length(resp$body), 4000) # This changes based on R ver/OS, may not be useful.
-  expect_lt(length(resp$body), 25000)
+  expect_gt(length(resp$body), 1000) # This changes based on R ver/OS, may not be useful.
 
   resp <- r$serve(make_req("GET", "/jpeg"), PlumberResponse$new())
-  expect_gt(length(resp$body), 4000) # This changes based on R ver/OS, may not be useful.
-  expect_lt(length(resp$body), 25000)
+  expect_gt(length(resp$body), 1000) # This changes based on R ver/OS, may not be useful.
 })
