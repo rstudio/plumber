42f4c23211f34b11b4b7707e32719b946d4a0ae8
frans <FvD@users.noreply.github.com>
2016-09-05 11:39:34 -0600

    add handling of qs formatted bodies

--- R/post-body.R
+++ R/post-body.R
@@ -9,6 +9,18 @@ postBodyFilter <- function(req){
 #' @importFrom utils URLdecode
 #' @noRd
 parseBody <- function(body){
+  # The body in a curl call can also include querystring formatted data
+  # Is there data in the request?
+  if (is.null(body) || length(body) == 0 || body == "") {
+    return(list())
+  }
+  # Is it JSON data?
+  if (stri_startswith_fixed(body, "{")) {
+    # Handle JSON with jsonlite
     ret <- jsonlite::fromJSON(body)
-    ret
+  } else {
+    # If not handle it as a query string
+      ret <- parseQS(body)
+  }
+  ret
 }
