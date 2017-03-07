177e0c02b7b8cb5541c1f000b721543ed3b3f73b
Thomas <teekaay@users.noreply.github.com>
2017-03-07 23:33:43 +0100

    Add auto_unbox for better JSON serialization

--- R/serializer-json.R
+++ R/serializer-json.R
@@ -3,7 +3,7 @@
 jsonSerializer <- function(){
   function(val, req, res, errorHandler){
     tryCatch({
-      json <- jsonlite::toJSON(val)
+      json <- jsonlite::toJSON(val, auto_unbox = TRUE)
 
       res$setHeader("Content-Type", "application/json")
       res$body <- json
