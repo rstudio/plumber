48a3fc991d7b77992bd7c2cb8f5b6ba8d0feb7fb
trestletech <jeff.allen@trestletechnology.net>
2017-04-05 22:03:42 -0500

    Get more recent visNetwork from source.

--- tests/testthat/test-serializer-htmlwidgets.R
+++ tests/testthat/test-serializer-htmlwidgets.R
@@ -2,13 +2,13 @@ context("htmlwidgets serializer")
 
 # Render a simple HTML widget using the visNetwork package
 renderWidget <- function(){
-  library(visNetwork)
   nodes <- data.frame(id = 1:6, title = paste("node", 1:6),
                       shape = c("dot", "square"),
                       size = 10:15, color = c("blue", "red"))
   edges <- data.frame(from = 1:5, to = c(5, 4, 6, 3, 3))
   visNetwork::visNetwork(nodes, edges) %>%
     visNetwork::visOptions(highlightNearest = TRUE, nodesIdSelection = TRUE)
+
 }
 
 test_that("htmlwidgets serialize properly", {
