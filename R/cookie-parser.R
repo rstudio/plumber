f57c06cf1d81f0f1cc3915e1460c519577a205af
trestletech <jeff.allen@trestletechnology.net>
2017-05-12 18:53:41 -0500

    Handle empty cookies.

--- R/cookie-parser.R
+++ R/cookie-parser.R
@@ -14,6 +14,14 @@ parseCookies <- function(cookie){
   cookie <- sub("\\s*([\\S*])\\s*", "\\1", cookie, perl=TRUE)
 
   cookieList <- strsplit(cookie, "=", fixed=TRUE)
+
+  # Handle any non-existent cookie values.
+  for (i in 1:length(cookieList)){
+    if(length(cookieList[[i]])==1){
+      cookieList[[i]][[2]] <- ""
+    }
+  }
+
   cookies <- lapply(cookieList, "[[", 2)
   names(cookies) <- sapply(cookieList, "[[", 1)
 
