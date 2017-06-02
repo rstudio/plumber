08b4a6bee56985e55f1c15b2a7d6dfd0a1172dd8
trestletech <jeff.allen@trestletechnology.net>
2017-06-01 21:57:56 -0500

    Pull apart functions in preparation for testing.

--- R/swagger.R
+++ R/swagger.R
@@ -14,13 +14,15 @@ plumberToSwaggerType <- function(type){
   }
 }
 
+#' Convert the endpoints as they exist on the router to a list which can
+#' be converted into a swagger definition for these endpoints
+#' @noRd
 prepareSwaggerEndpoints <- function(routerEndpoints){
   endpoints <- list()
 
   for (fil in routerEndpoints){
     for (e in fil){
       # TODO: we are sensitive to trailing slashes. Should we be?
-      # cleanedPath <- gsub("/$", "", e$path)
       cleanedPath <- gsub("<([^:>]+)(:[^>]+)?>", "{\\1}", e$path)
       if (is.null(endpoints[[cleanedPath]])){
         endpoints[[cleanedPath]] <- list()
@@ -29,62 +31,71 @@ prepareSwaggerEndpoints <- function(routerEndpoints){
       # Get the params from the path
       pathParams <- e$getTypedParams()
       for (verb in e$verbs){
-        params <- data.frame(name=character(0),
-                             description=character(0),
-                             `in`=character(0),
-                             required=logical(0),
-                             type=character(0), check.names = FALSE)
-
-        for (p in names(e$params)){
-          location <- "query"
-          if (p %in% pathParams$name){
-            location <- "path"
-          }
-
-          type <- e$params[[p]]$type
-          if (is.na(type)){
-            if (location == "path") {
-              type <- pathParams[pathParams$name == p,"type"]
-            } else {
-              type <- "string" # Default to string
-            }
-          }
-
-          parDocs <- data.frame(name = p,
-                                description = e$params[[p]]$desc,
-                                `in`=location,
-                                type=type,
-                                required=e$params[[p]]$required,
-                                check.names = FALSE,
-                                required=FALSE)
-
-          if (location == "path"){
-            parDocs$required <- TRUE
-          }
-
-          params <- rbind(params, parDocs)
-        }
+        params <- extractSwaggerParams(e$params, pathParams)
 
         # If we haven't already documented a path param, we should add it here.
         # FIXME: warning("Undocumented path parameters: ", paste0())
 
-        resps <- e$responses
-        defaultResp <- list("default"=list(description="Default response."))
-        if (is.null(resps)){
-          resps <- defaultResp
-        } else if (!("default" %in% names(resps))){
-          resps <- c(resps, defaultResp)
-        }
+        resps <- extractResponses(e$responses)
 
         endptSwag <- list(summary=e$comments,
                           responses=resps,
                           parameters=params)
 
         endpoints[[cleanedPath]][[tolower(verb)]] <- endptSwag
-
       }
     }
   }
 
   endpoints
+}
+
+extractResponses <- function(resps){
+  defaultResp <- list("default"=list(description="Default response."))
+  if (is.null(resps)){
+    resps <- defaultResp
+  } else if (!("default" %in% names(resps))){
+    resps <- c(resps, defaultResp)
+  }
+  resps
+}
+
+#' Extract the swagger-friendly parameter definitions from the endpoint
+#' paramters.
+extractSwaggerParams <- function(endpointParams, pathParams){
+  params <- data.frame(name=character(0),
+                       description=character(0),
+                       `in`=character(0),
+                       required=logical(0),
+                       type=character(0),
+                       check.names = FALSE)
+  for (p in names(endpointParams)){
+    location <- "query"
+    if (p %in% pathParams$name){
+      location <- "path"
+    }
+
+    type <- endpointParams[[p]]$type
+    if (is.na(type)){
+      if (location == "path") {
+        type <- pathParams[pathParams$name == p,"type"]
+      } else {
+        type <- "string" # Default to string
+      }
+    }
+
+    parDocs <- data.frame(name = p,
+                          description = endpointParams[[p]]$desc,
+                          `in`=location,
+                          required=endpointParams[[p]]$required,
+                          type=type,
+                          check.names = FALSE)
+
+    if (location == "path"){
+      parDocs$required <- TRUE
+    }
+
+    params <- rbind(params, parDocs)
+  }
+  params
 }
--- tests/testthat/test-swagger.R
+++ tests/testthat/test-swagger.R
@@ -1,10 +1,26 @@
 context("swagger")
 
+test_that("plumberToSwaggerType works", {
+  testthat::fail()
+})
+
 test_that("response attributes are parsed", {
   testthat::fail()
 })
 
 test_that("params are parsed", {
   # Name, type, required, description
+  testthat::fail()
+})
+
+test_that("prepareSwaggerEndpoints works", {
+  testthat::fail()
+})
+
+test_that("extractResponses works", {
+  testthat::fail()
+})
+
+test_that("extractSwaggerParams works", {
   testthat::fail()
 })
