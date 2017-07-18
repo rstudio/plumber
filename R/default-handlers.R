b58e5c40f1e507cd57f787310e8ad9d6808efc8b
trestletech <jeff.allen@trestletechnology.net>
2017-07-18 15:54:54 -0500

    Tests for default handlers.

--- R/default-handlers.R
+++ R/default-handlers.R
@@ -8,14 +8,18 @@ defaultErrorHandler <- function(debug=FALSE){
   function(req, res, err){
     print(err)
 
+    li <- list()
+
     if (res$status == 200L){
       # The default is a 200. If that's still set, then we should probably override with a 500.
       # It's possible, however, than a handler set a 40x and then wants to use this function to
       # render an error, though.
       res$status <- 500
+      li$error <- "500 - Internal server error"
+    } else {
+      li$error <- "Internal error"
     }
 
-    li <- list(error="500 - Internal server error")
 
     # Don't overly leak data unless they opt-in
     if (debug){
