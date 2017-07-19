21db8e6bfc23e4c8436f06a8c783d4202748aa07
trestletech <jeff.allen@trestletechnology.net>
2017-07-19 16:13:10 -0500

    Don't wildcard allow-origin by default.
    This could be considered a vulnerability in how Plumber behaved previously. This, by default, allows cross-origin GET, HEAD, and POST requests using the standard headers from any origin. We've decided that this is something you should opt-in to, so we're removing this as the default.

--- tests/testthat/test-response.R
+++ tests/testthat/test-response.R
@@ -38,9 +38,3 @@ test_that("can set multiple same-named headers", {
   expect_true(another)
 })
 
-test_that("doesn't overwrite CORS", {
-  res <- PlumberResponse$new()
-  res$setHeader("Access-Control-Allow-Origin", "originhere")
-  head <- res$toResponse()$headers
-  expect_equal(head[["Access-Control-Allow-Origin"]], "originhere")
-})
