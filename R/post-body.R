8a1a1089f665a3ce4766c13201e424f88eee54fa
frans <FvD@users.noreply.github.com>
2017-07-29 08:16:15 -0600

    Add check for the availability of webutils package

--- R/post-body.R
+++ R/post-body.R
@@ -15,6 +15,7 @@ postBodyFilter <- function(req){
   forward()
 }
 
+
 #' @importFrom utils URLdecode
 #' @noRd
 parseBody <- function(body){
@@ -34,28 +35,32 @@ parseBody <- function(body){
   ret
 }
 
+
 #' @importFrom utils URLdecode
 #' @noRd
 parseMultipart <- function(body, boundary){
-  # This is not as generic as perhaps it should be, but should be relatively
-  # easy to extend if more binary formats are required
-  # Is there data in the request?
-  if (is.null(body) || length(body) == 0 || body == "") {
-    return(list())
+  function(val, req, res, errorHandler){
+    tryCatch({
+      if (!requireNamespace("webutils", quietly = TRUE)) {
+        stop("The webutils package is not available but is required in order
+          to use the functionality to POST binary files",
+             call. = FALSE)
+      }
+      # Is there data in the request?
+      if (is.null(body) || length(body) == 0 || body == "") {
+        return(list())
+      }
+      parsed_binary <- webutils::parse_multipart(body, boundary)
+      file_name <- parsed_binary$myfile$filename
+      file_data <- parsed_binary$myfile$value
+
+      tmpfile <- tempfile()
+      writeBin(file_data, tmpfile)
+
+      ret <- NULL
+      ret$name <- file_name
+      ret$data <- tmpfile
+      ret
+    })
   }
-  # This is something odd with webutils::parse_multipart. For some reason it
-  # does not work when the boundary is set to the content type as per the
-  # webutils docs.
-  parsed_binary <- webutils::parse_multipart(body, boundary)
-#  file_name = "test.feather"
-  file_name <- parsed_binary$myfile$filename
-  file_data <- parsed_binary$myfile$value
-
-  tmpfile <- tempfile()
-  writeBin(file_data, tmpfile)
-
-  ret <- NULL
-  ret$name <- file_name
-  ret$data <- tmpfile
-  ret
 }
