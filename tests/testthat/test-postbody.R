ea1885988c665d5391d3aabea62b131adc4dc768
Jeremy Farrell <farrjere@isu.edu>
2018-05-03 00:03:16 -0600

    adding test for encoding

--- tests/testthat/test-postbody.R
+++ tests/testthat/test-postbody.R
@@ -8,3 +8,7 @@ test_that("Query strings on post are handled correctly", {
   expect_equivalent(parseBody("a="), list()) # It's technically a named list()
   expect_equal(parseBody("a=1&b=&c&d=1"), list(a="1", d="1"))
 })
+
+test_that("Able to handle UTF-8", {
+  expect_equal(parseBody('{"Json" :{"text":"Élise"}}')$text, "Élise")
+})
