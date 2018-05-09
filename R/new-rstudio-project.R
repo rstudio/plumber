324dfa0de642939cf9a59c6be3332b6ecb355628
Gary <gary@rstudio.com>
2018-05-09 16:53:29 -0700

    copy over plumber.R instead of writing via code

--- R/new-rstudio-project.R
+++ R/new-rstudio-project.R
@@ -12,51 +12,12 @@ newRStudioProject <- function(path, ...) {
   # ensure path exists
   dir.create(path, recursive = TRUE, showWarnings = FALSE)
 
-  # generate plumber.R
-  sample <- c(
-    "#",
-    "# This is a Plumber API. In RStudio 1.2 or newer you can run the API by",
-    "# clicking the 'Run API' button above.",
-    "#",
-    "# In RStudio 1.1 or older, see the Plumber documentation for details",
-    "# on running the API.",
-    "#",
-    "# Find out more about building APIs with Plumber here:",
-    "#",
-    "#    https://www.rplumber.io/",
-    "#",
-    "",
-    "library(plumber)",
-    "",
-    "#* @apiTitle Plumber Example API",
-    "",
-    "#* Echo back the input",
-    "#* @param msg The message to echo",
-    "#* @get /echo",
-    "function(msg=\"\"){",
-    "  list(msg = paste0(\"The message is: '\", msg, \"'\"))",
-    "}",
-    "",
-    "#* Plot a histogram",
-    "#* @png",
-    "#* @get /plot",
-    "function(){",
-    "  rand <- rnorm(100)",
-    "  hist(rand)",
-    "}",
-    "",
-    "#* Return the sum of two numbers",
-    "#* @param a The first number to add",
-    "#* @param b The second number to add",
-    "#* @post /sum",
-    "function(a, b){",
-    "  as.numeric(a) + as.numeric(b)",
-    "}",
-    ""
-  )
+  # copy 'resources' folder to path
+  resources = system.file("rstudio", "templates", "project", "resources",
+                           package = "plumber", mustWork = TRUE)
 
-  contents <- paste(sample, collapse = "\n")
-
-  # write to index file
-  writeLines(contents, con = file.path(path, "plumber.R"))
+  files = list.files(resources, recursive = TRUE, include.dirs = FALSE)
+  source = file.path(resources, files)
+  target = file.path(path, files)
+  file.copy(source, target)
 }
