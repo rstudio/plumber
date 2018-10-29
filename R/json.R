79f7461d52184662853fe15e74e80ff8ec59a050
Barret Schloerke <schloerke@gmail.com>
2018-10-29 10:43:22 -0400

    remove importFrom's and else from safeFromJSON

--- R/json.R
+++ R/json.R
@@ -1,9 +1,7 @@
-#' @importFrom jsonlite validate
-#' @importFrom jsonlite fromJSON
 safeFromJSON <- function(txt, ...){
   if (!jsonlite::validate(txt)){
     stop("Argument 'txt' is not a valid JSON string.")
-  } else {
-    jsonlite::fromJSON(txt, ...)
   }
+  
+  jsonlite::fromJSON(txt, ...)
 }
