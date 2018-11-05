9a3a40f09f2a9b0d7a6e7642a15d55ee5964c43e
Barret Schloerke <schloerke@gmail.com>
2018-11-05 10:00:32 -0500

    add comments about LC_ALL and use charToRaw to test UTF-8 strings

--- tests/testthat/test-multibytes.R
+++ tests/testthat/test-multibytes.R
@@ -15,6 +15,11 @@ expect_utf8 <- function(x) {
 
 test_that("support files with multibytes", {
 
+  # LC_ALL was not used as Mac had warnings when trying to restore LC_ALL
+  # ?Sys.setlocale
+  # `R sets ‘"LC_CTYPE"’ and ‘"LC_COLLATE"’,
+  #  which allow the use of a different character set and alphabetic
+  #  comparisons in that character set`
   with_locale("LC_CTYPE", "C", {
     with_locale("LC_COLLATE", "C", {
 
@@ -24,15 +29,20 @@ test_that("support files with multibytes", {
       out <- r$serve(req, res)$body
 
       # ?Quotes
-      # "Unicode escapes can be used to enter Unicode characters not in the current locale's
-      # charset (when the string will be stored internally in UTF-8)."
+      # `Unicode escapes can be used to enter Unicode characters not in the current locale's
+      # charset (when the string will be stored internally in UTF-8).`
       expected <- jsonlite::toJSON("\u4e2d\u6587\u6d88\u606f")
 
       expect_identical(Sys.getlocale("LC_CTYPE"), "C")
       expect_identical(Sys.getlocale("LC_COLLATE"), "C")
       expect_utf8(expected)
       expect_utf8(out)
-      expect_identical(out, expected)
+      # using charToRaw as identical coerces strings to different locales
+      # ?identical
+      # `Character strings are regarded as identical if they are in
+      # different marked encodings but would agree when translated to
+      # UTF-8.`
+      expect_identical(charToRaw(out), charToRaw(expected))
 
     })
   })
