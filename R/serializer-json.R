ca100fb43f206a32a86b5961faf1ba59afffa551
Barret Schloerke <schloerke@gmail.com>
2018-12-07 11:04:37 -0500

    drop serializer_json purrr implementation and use ... instead

--- R/serializer-json.R
+++ R/serializer-json.R
@@ -1,40 +1,28 @@
 #' @include globals.R
 #' @rdname serializers
-#' @param na How should NA values be encoded?
+#' @param ... extra arguments supplied to respective internal serialization function
 #' @export
-serializer_json <- function(na = "null"){
-  purrr::partial(function(val, req, res, errorHandler, na){
-
+serializer_json <- function(...) {
+  function(val, req, res, errorHandler) {
     tryCatch({
-      json <- jsonlite::toJSON(val, na = na)
+      json <- jsonlite::toJSON(val, ...)
 
       res$setHeader("Content-Type", "application/json")
       res$body <- json
 
       return(res$toResponse())
-    }, error=function(e){
+    }, error = function(e){
       errorHandler(req, res, e)
     })
-  }, na = na, .lazy = FALSE)
+  }
 }
 .globals$serializers[["json"]] <- serializer_json
 
 #' @include globals.R
 #' @rdname serializers
 #' @export
-serializer_unboxed_json <- function(na = "null"){
-  purrr::partial(function(val, req, res, errorHandler, na){
-    tryCatch({
-      json <- jsonlite::toJSON(val, na = na, auto_unbox = TRUE)
-
-      res$setHeader("Content-Type", "application/json")
-      res$body <- json
-
-      return(res$toResponse())
-    }, error=function(e){
-      errorHandler(req, res, e)
-    })
-  }, na = na, .lazy = FALSE)
+serializer_unboxed_json <- function(auto_unbox = TRUE, ...) {
+  serializer_json(auto_unbox = auto_unbox, ...)
 }
 
 .globals$serializers[["unboxedJSON"]] <- serializer_unboxed_json
