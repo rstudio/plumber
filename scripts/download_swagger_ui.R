37c7f232bda98927cd389c817de5d076113c25f2
Barret Schloerke <schloerke@gmail.com>
2018-12-18 10:40:29 -0500

    add download script for swagger-ui

---
+++ scripts/download_swagger_ui.R
@@ -0,0 +1,25 @@
+
+
+swagger_ui_version <- "3.20.2"
+to_location <- file.path("inst", "swagger_ui")
+
+tmp_location <- tempdir()
+
+withr::with_dir(devtools::as.package(".")$path, {
+  unlink(to_location, recursive = TRUE)
+  dir.create(to_location, recursive = TRUE)
+  system(paste0("wget -r -p -np -l 15 -nH -P ", tmp_location, " https://unpkg.com/swagger-ui-dist@", swagger_ui_version, "/"))
+
+  file.path(tmp_location, paste0("swagger-ui-dist@", swagger_ui_version), "") %>%
+    dir(full.names = TRUE) %>%
+    lapply(file.copy, to = to_location)
+})
+
+unlink(tmp_location)
+
+# make sure the petstore swagger call is replaced with local swagger.json call
+indexFile <- file.path(to_location, "index.html")
+indexFile %>%
+  readLines() %>%
+  sub("https://petstore.swagger.io/v2/swagger.json", "/swagger.json", ., fixed = TRUE) %>%
+  writeLines(indexFile)
