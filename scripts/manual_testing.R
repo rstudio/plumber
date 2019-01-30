987a7c57951d6b5f10944bcea2624a530ff57f69
Barret Schloerke <schloerke@gmail.com>
2019-01-30 10:31:33 -0500

    add a manual test

--- scripts/manual_testing.R
+++ scripts/manual_testing.R
@@ -22,3 +22,20 @@ test_that("custom swagger file update function works", {
   # validate that http://127.0.0.1:1234/__swagger__/ displays the system time as the api title
   # http://127.0.0.1:1234/__swagger__/
 })
+
+
+
+
+test_that("host doesn't change for messages, but does for RStudio IDE", {
+
+  pr <- plumber$new()
+  pr$handle("GET", "/:path/here", function(){})
+
+  pr$run(
+    "0.0.0.0", port = 1234
+  )
+
+  # verify that a 0.0.0.0 host is printed in all messages
+  # if in RStudio IDE, verify that the window opened, opens to http://127.0.0.1:1234/__swagger__/
+  # verify that the swagger route (from messages) works in a web browser http://0.0.0.0:1234/__swagger__/
+})
