83c8faaf1766f20d1a164182cdc72b222b2bfaed
Barret Schloerke <schloerke@gmail.com>
2019-02-22 16:11:54 -0500

    update serializer rds test for check v2 and v3

--- tests/testthat/test-serializer-rds.R
+++ tests/testthat/test-serializer-rds.R
@@ -1,11 +1,26 @@
-context("rObject serializer")
+context("rds serializer")
 
-test_that("rObject serializes properly", {
+test_that("rds serializes properly", {
   v <- iris[0,]
   attr(v, "origin") <- iris
-  val <- serializer_r_object()(v, list(), PlumberResponse$new(), stop)
+  val <- serializer_rds()(v, list(), PlumberResponse$new(), stop)
   expect_equal(val$status, 200L)
   expect_equal(val$headers$`Content-Type`, "application/octet-stream")
   expect_equal(val$body, serialize(v, NULL, ascii = FALSE))
+  expect_equal(unserialize(val$body), v)
+})
+
+
+test_that("rds3 serializes properly", {
+
+  testthat::skip_if(package_version(R.version) < "3.5")
+
+  v <- iris[0,]
+  attr(v, "origin") <- iris
+  # version 3 added in R 3.5
+  val <- serializer_rds3()(v, list(), PlumberResponse$new(), stop)
+  expect_equal(val$status, 200L)
+  expect_equal(val$headers$`Content-Type`, "application/octet-stream")
+  expect_equal(val$body, serialize(v, NULL, ascii = FALSE, version = "3"))
   expect_equal(unserialize(val$body), v)
 })
