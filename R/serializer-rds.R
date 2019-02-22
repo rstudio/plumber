e219962d4e43bfb1cc9952e77df8a954081f8c75
Barret Schloerke <schloerke@gmail.com>
2019-02-22 15:56:24 -0500

    add serializer_rds3

--- R/serializer-rds.R
+++ R/serializer-rds.R
@@ -14,3 +14,10 @@ serializer_rds <- function(version = "2", ascii = FALSE, ...) {
 }
 
 .globals$serializers[["rObject"]] <- serializer_r_object
+
+#' @rdname serializers
+#' @inheritParams base::serialize
+#' @export
+serializer_rds3 <- function(version = "3", ascii = FALSE, ...) {
+  serializer_rds(version = version, ascii = ascii, ...)
+}
