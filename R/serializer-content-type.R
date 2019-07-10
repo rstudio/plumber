bca21aa5f837002d5ffcdb52121da41dfef398d5
ycphs <philipp@schauberger.co.at>
2019-07-10 19:59:22 +0200

    changed method

--- R/serializer-content-type.R
+++ R/serializer-content-type.R
@@ -1,17 +1,35 @@
 #' @rdname serializers
 #' @param type The value to provide for the `Content-Type` HTTP header.
+#' @param disposition The value to provide for the `Disposition` HTTP header.
+#' @param filename The value to provide for the `Filename`.
 #' @export
-serializer_content_type <- function(type) {
-  if (missing(type)){
-    stop("You must provide the custom content type to the serializer_content_type")
+serializer_content_type <- function(type, disposition = NULL, filename = NULL) {
+  if (missing(type)) {
+    stop("You must provide the custom content type to the serializer_content_type_disposition")
   }
-  function(val, req, res, errorHandler){
+
+  # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Disposition
+  if (!is.null(disposition)) {
+    disposition <- match.arg(disposition, c("inline", "attachment"))
+    if (disposition == "attachment" && !is.null(filename)) {
+      filename <- as.character(filename)
+      if (grepl("\"", filename, fixed = TRUE)) {
+        stop("quotes can not be used in the value of `filename`")
+      }
+      disposition <- paste0(disposition, "; filename=\"", filename, "\"")
+    }
+  }
+
+  function(val, req, res, errorHandler) {
     tryCatch({
       res$setHeader("Content-Type", type)
+      if (!is.null(disposition)) {
+        res$setHeader("Content-Disposition", disposition)
+      }
       res$body <- val
 
       return(res$toResponse())
-    }, error=function(e){
+    }, error=function(e) {
       errorHandler(req, res, e)
     })
   }
