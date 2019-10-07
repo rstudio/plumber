3a81b281324541f199582821c09d70c4debbc0fd
Barret Schloerke <schloerke@gmail.com>
2019-10-07 13:37:45 -0400

    add hello world future example with notes

--- inst/examples/14-future/plumber.R
+++ inst/examples/14-future/plumber.R
@@ -1,22 +1,34 @@
 
-# Visit /future
-# While /future is loading, visit /sync many times
-
 library(promises)
 library(future)
 
-future::plan("multiprocess")
+future::plan("multiprocess") # use all available cores
+# future::plan(future::multiprocess(workers = 2)) # only two cores
+
+# Quick manual test:
+# Within 10 seconds...
+# 1. Visit /future
+# 2. While /future is loading, visit /sync many times
+# /future will not block /sync from being able to be loaded.
 
 
+#' @json(auto_unbox = TRUE)
 #' @get /sync
 function() {
-  paste0("sync: ", Sys.time())
+  # print route, time, and worker pid
+  paste0("/sync; ", Sys.time(), "; pid:", Sys.getpid())
 }
 
+#' @contentType list(type = "text/html")
+#' @json(auto_unbox = TRUE)
 #' @get /future
 function() {
+
   future({
+    # perform large computations
     Sys.sleep(10)
-    paste0("future: ", Sys.time())
+
+    # print route, time, and worker pid
+    paste0("/future; ", Sys.time(), "; pid:", Sys.getpid())
   })
 }
