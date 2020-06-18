d726f15c76fe323c42b60c1f2ccf7c2e09185382
Pachamaltese <mvargas@dcc.uchile.cl>
2020-06-18 11:51:31 -0400

    Update tests/testthat/test-serializer-csv.R
    Co-authored-by: Barret Schloerke <barret@rstudio.com>

--- tests/testthat/test-serializer-csv.R
+++ tests/testthat/test-serializer-csv.R
@@ -18,7 +18,7 @@ test_that("CSV serializes properly", {
   d <- data.frame(a=1, b=2, c="hi", na=NA)
   val <- serializer_csv(na = 'string')(d, data.frame(), PlumberResponse$new(), stop)
   expect_equal(val$status, 200L)
-  expect_equal(val$headers$`Content-Type`, "text/plain; charset=utf-8")
+  expect_equal(val$headers$`Content-Type`, "text/plain; charset=UTF-8")
   expect_equal(val$body, readr::format_csv(d, na = 'string'))
 })
 
