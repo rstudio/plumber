9a4323dc86001daa247076904c6c0e2b3d6cc3cb
Bruno Tremblay <bruno.tremblay@lacapitale.com>
2020-06-23 17:31:35 -0400

    update test

--- tests/testthat/test-options.R
+++ tests/testthat/test-options.R
@@ -2,10 +2,9 @@ context("Options")
 
 test_that("Options set and get", {
   option_value <- getOption("plumber.postBody")
-  optionsPlumber(postBody = FALSE)
+  options_plumber(postBody = FALSE)
   expect_false(getOption("plumber.postBody"))
-  expect_false(optionsPlumber()$plumber.postBody)
-  optionsPlumber(postBody = NULL)
+  options_plumber(postBody = NULL)
   expect_null(getOption("plumber.postBody"))
   options(plumber.postBody = option_value)
 })
