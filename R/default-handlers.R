a7e709a4da905ec8a72786187fd1fb62e190e037
Bruno Tremblay <meztez@neoxone.com>
2020-06-23 14:11:02 -0400

    Update R/default-handlers.R
    Co-authored-by: Barret Schloerke <barret@rstudio.com>

--- R/default-handlers.R
+++ R/default-handlers.R
@@ -1,7 +1,7 @@
 #' @include plumber.R
 default404Handler <- function(req, res){
   if (isTRUE(req$methodNotAllowed)) {
-    res$status = 405L
+    res$status <- 405L
     # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Allow
     res$setHeader("Allow", paste(req$verbsAllowed, collapse = ", "))
     return(list(error = "405 - Method Not Allowed"))
