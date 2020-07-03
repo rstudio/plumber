11d459c5bedbaab06db409c616486cf6d0f08ad4
Barret Schloerke <schloerke@gmail.com>
2020-07-03 13:45:15 -0400

    Add tsv and csv tests. Rename test file.

--- tests/testthat/test-parse-body.R
+++ tests/testthat/test-parse-body.R
@@ -56,7 +56,32 @@ test_that("Test yaml parser", {
   expect_equal(parse_body(charToRaw(yaml::as.yaml(r_object)), "application/x-yaml"), r_object)
 })
 
+test_that("Test csv parser", {
+  tmp <- tempfile()
+  on.exit({
+    file.remove(tmp)
+  }, add = TRUE)
+
+  r_object <- cars
+  write.csv(r_object, tmp, row.names = FALSE)
+  val <- readBin(tmp, "raw", 1000)
+  expect_equal(parse_body(val, "application/csv"), r_object)
+})
+
+test_that("Test tsv parser", {
+  tmp <- tempfile()
+  on.exit({
+    file.remove(tmp)
+  }, add = TRUE)
+
+  r_object <- cars
+  write.table(r_object, tmp, sep = "\t", row.names = FALSE)
+  val <- readBin(tmp, "raw", 1000)
+  expect_equal(parse_body(val, "application/tab-separated-values"), r_object)
+})
+
 test_that("Test multipart parser", {
+  # also tests rds and the octet -> content type conversion
 
   bin_file <- test_path("files/multipart-form.bin")
   body <- readBin(bin_file, what = "raw", n = file.info(bin_file)$size)
@@ -68,9 +93,10 @@ test_that("Test multipart parser", {
   expect_equal(parsed_body[["json"]], list(a=2,b=4,c=list(w=3,t=5)))
 })
 
+
 test_that("Test multipart respect content-type", {
   bin_file <- test_path("files/multipart-ctype.bin")
   body <- readBin(bin_file, what = "raw", n = file.info(bin_file)$size)
   parsed_body <- parse_body(body, "multipart/form-data; boundary=---------------------------90908882332870323642673870272")
-  expect_equal(class(parsed_body$file), "character")
+  expect_s3_class(parsed_body$file, "data.frame")
 })
