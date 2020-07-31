16e01a9ef0dc2d059e33f1826469a7e0ccbad73c
Barret Schloerke <barret@rstudio.com>
2020-07-31 10:54:18 -0400

    Forgot to add `function` in the pseudo docs
    Co-authored-by: Carson Sievert <cpsievert1@gmail.com>

--- R/parse-body.R
+++ R/parse-body.R
@@ -118,7 +118,7 @@ parser_picker <- function(content_type, first_byte, filename = NULL, parsers = N
 #'
 #' Parser function structure is something like below.
 #' ```r
-#' parser <- (parser_arguments_here) {
+#' parser <- function(parser_arguments_here) {
 #'   # return a function to parse a raw value
 #'   function(value, ...) {
 #'     # do something with raw value
