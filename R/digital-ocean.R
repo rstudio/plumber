5705f46fcdb77dafac0e0682d194b2c19d904ca7
John Muschelli <muschellij2@gmail.com>
2020-08-12 11:25:30 -0400

    Added longer gpg key 51716619E084DAB9 to avoid collisions (#645)
    Co-authored-by: muschellij2 <muschellij2@gmail.com>

--- R/digital-ocean.R
+++ R/digital-ocean.R
@@ -151,7 +151,7 @@ install_nginx <- function(droplet){
 }
 
 install_new_r <- function(droplet){
-  analogsea::droplet_ssh(droplet, "apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9")
+  analogsea::droplet_ssh(droplet, "apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 51716619E084DAB9")
   analogsea::droplet_ssh(droplet, "echo 'deb https://cran.rstudio.com/bin/linux/ubuntu focal-cran40/' >> /etc/apt/sources.list.d/cran.list")
   # TODO: use the analogsea version once https://github.com/sckott/analogsea/issues/139 is resolved
   #analogsea::debian_apt_get_update(droplet)
