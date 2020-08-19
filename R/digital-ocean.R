9104025113b8328dbdeabb70d90938f5a7b55d65
Barret Schloerke <schloerke@gmail.com>
2020-08-19 16:49:51 -0400

    Use a helper method to orchestrate to call methods or throw warnings

--- R/digital-ocean.R
+++ R/digital-ocean.R
@@ -8,35 +8,56 @@
 #' @rdname digitalocean
 #' @export
 do_provision <- function(...) {
-  lifecycle::deprecate_stop("1.0.0", "do_provision()", "plumberDeploy::do_provision()")
+  plumber_deploy_helper("do_provision", list(...))
 }
 
 #' @export
 #' @rdname digitalocean
 do_configure_https <- function(...) {
-  lifecycle::deprecate_stop("1.0.0", "do_configure_https()", "plumberDeploy::do_configure_https()")
+  plumber_deploy_helper("do_configure_https", list(...))
 }
 
 #' @export
 #' @rdname digitalocean
 do_deploy_api <- function(...) {
-  lifecycle::deprecate_stop("1.0.0", "do_deploy_api()", "plumberDeploy::do_deploy_api()")
+  plumber_deploy_helper("do_deploy_api", list(...))
 }
 
 #' @export
 #' @rdname digitalocean
 do_forward <- function(...) {
-  lifecycle::deprecate_stop("1.0.0", "do_forward()", "plumberDeploy::do_forward()")
+  plumber_deploy_helper("do_forward", list(...))
 }
 
 #' @export
 #' @rdname digitalocean
 do_remove_api <- function(...) {
-  lifecycle::deprecate_stop("1.0.0", "do_remove_api()", "plumberDeploy::do_remove_api()")
+  plumber_deploy_helper("do_remove_api", list(...))
 }
 
 #' @export
 #' @rdname digitalocean
 do_remove_forward <- function(...) {
-  lifecycle::deprecate_stop("1.0.0", "do_remove_forward()", "plumberDeploy::do_remove_forward()")
+  plumber_deploy_helper("do_remove_forward", list(...))
+}
+
+
+plumber_deploy_helper <- function(fn_name, args, new_fn_name = fn_name) {
+  cur_fn <- paste0(fn_name, "()")
+  new_fn <- paste0("plumberDeploy::", new_fn_name, "()")
+
+  # check if plumberDeploy is called
+  if (!requireNamespace("plumberDeploy", quietly = TRUE)) {
+    # not found
+    # throw error
+    lifecycle::deprecate_stop("1.0.0", cur_fn, new_fn)
+  }
+
+  # plumberDeploy is found
+  # 1. Throw warning
+  # 2. Call method
+  lifecycle::deprecate_warn("1.0.0", cur_fn, new_fn)
+
+  fn <- getFromNamespace(fn_name, "plumberDeploy")
+  do.call(fn, args)
 }
