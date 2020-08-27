7bd66bac8efdd5688a539a898f5be9472fed5542
Barret Schloerke <schloerke@gmail.com>
2020-08-27 12:05:34 -0400

    Update tests to test for new multipart structures

--- tests/testthat/test-parser.R
+++ tests/testthat/test-parser.R
@@ -67,8 +67,7 @@ test_that("parsers work", {
     })
   expect_equal(names(parsed_body), c("json", "img1", "img2", "rds"))
   expect_equal(parsed_body[["rds"]], women)
-  expect_equal(names(parsed_body[["img1"]]), c("avatar2-small.png"))
-  expect_true(is.raw(parsed_body[["img1"]][["avatar2-small.png"]]))
+  expect_true(is.raw(parsed_body[["img1"]]))
   expect_equal(parsed_body[["json"]], list(a=2,b=4,c=list(w=3,t=5)))
 
 
