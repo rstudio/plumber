94f3e8124f96b868a82264ffb7ba1589c7e194e0
Barret Schloerke <schloerke@gmail.com>
2020-10-19 16:08:37 -0400

    Update ui.R

--- R/ui.R
+++ R/ui.R
@@ -22,6 +22,7 @@ mount_docs <- function(pr, host, port, docs_info, callback) {
   )
 
   # Mount openAPI spec paths openapi.json
+cat("Mounting openpi")
   mount_openapi(pr, api_url)
 
   # Mount Docs
@@ -32,6 +33,7 @@ mount_docs <- function(pr, host, port, docs_info, callback) {
 
   if (is_docs_available(docs_info$docs)) {
     docs_mount <- .globals$docs[[docs_info$docs]]$mount
+cat("Attempt to set up docs: ", docs_info$docs, "\n")
     docs_url <- do.call(docs_mount, c(list(pr, api_url), docs_info$args))
     message("Running ", docs_info$docs, " Docs at ", docs_url, sep = "")
   } else {
@@ -182,9 +184,9 @@ register_docs <- function(name, index, static = NULL) {
 
   docs_root <- paste0("/__docs__/")
   docs_paths <- c("/index.html", "/")
-
+cat("Registering docs: ", name, "\n")
   mount_docs_func <- function(pr, api_url, ...) {
-
+cat("Calling docs fn: ", name, "\n")
     # Save initial extra argument values
     args_index <- list(...)
 
@@ -209,16 +211,18 @@ register_docs <- function(name, index, static = NULL) {
       message("")
     }
 
+cat("Mounting docs_root: ", name, ", ", docs_root, "\n")
     pr$mount(docs_root, docs_router)
 
     # add legacy swagger redirects (RStudio Connect)
     redirect_info <- swagger_redirects()
+str(redirect_info)
     for (path in names(redirect_info)) {
-      cat("testing route: ", path, "\n")
+cat("testing route: ", path, "\n")
       if (router_has_route(pr, path, "GET")) {
         message("Overwriting existing GET endpoint: ", path, ". Disable by setting `options_plumber(legacyRedirects = FALSE)`")
       }
-      cat("testing route: ", redirect_info[[path]], "\n")
+cat("testing route: ", redirect_info[[path]], "\n")
       if (router_has_route(pr, redirect_info[[path]], "GET")) {
         message("Overwriting existing GET endpoint: ", redirect_info[[path]], ". Disable by setting `options_plumber(legacyRedirects = FALSE)`")
       }
