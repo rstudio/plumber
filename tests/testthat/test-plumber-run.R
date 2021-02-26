755b71f1ac288fff5b565a6320ee8cc1361acea5
Barret Schloerke <schloerke@gmail.com>
2021-02-26 15:52:30 -0500

    Test swaggerCallback option can be set after init

--- tests/testthat/test-plumber-run.R
+++ tests/testthat/test-plumber-run.R
@@ -39,28 +39,55 @@ test_that("`docs` does not not permanetly set pr information", {
 })
 
 test_that("`swaggerCallback` does not not permanetly set pr information", {
-  msg <- "test got here!"
+  counter <- 0
   my_func <- function(url) {
-    message(msg)
+    counter <<- counter + 1
   }
   root <- pr() %>% pr_set_docs_callback(my_func)
+  # not used
   with_interrupt({
-    expect_failure(
-      expect_message({
-        root %>% pr_run(swaggerCallback = NULL)
-      }, msg)
-    )
+    expect_equal(counter, 0)
+    root %>% pr_run(swaggerCallback = NULL)
+    expect_equal(counter, 0)
   })
+  # not used
   with_interrupt({
-    expect_failure(
-      expect_message({
-        root$run(swaggerCallback = NULL)
-      }, msg)
-    )
+    expect_equal(counter, 0)
+    root$run(swaggerCallback = NULL)
+    expect_equal(counter, 0)
   })
+  # used
   with_interrupt({
-    expect_message({
-      root %>% pr_run(quiet = FALSE)
-    }, msg)
+    expect_equal(counter, 0)
+    root %>% pr_run(quiet = FALSE)
+    expect_equal(counter, 1)
   })
+})
+
+test_that("`swaggerCallback` can be set by option after the pr is created", {
+  counter <- 0
+  my_func <- function(url) {
+    counter <<- counter + 1
+  }
+
+  # must initialize before options are set
+  root <- pr()
+
+  with_options(
+    list(
+      plumber.swagger.url = getOption("plumber.swagger.url"),
+      plumber.docs.callback = getOption("plumber.docs.callback")
+    ),
+    {
+      # set option after init
+      options_plumber(docs.callback = my_func)
+      with_interrupt({
+        expect_equal(counter, 0)
+        pr_run(root)
+      })
+    }
+  )
+  # used
+  expect_equal(counter, 1)
+
 })
