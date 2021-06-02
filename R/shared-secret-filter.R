bacf6ea94c7f52756e11d257447527593335149b
Barret Schloerke <barret@rstudio.com>
2021-06-02 17:37:13 -0400

    Force an unboxed json serializer

--- R/shared-secret-filter.R
+++ R/shared-secret-filter.R
@@ -5,6 +5,8 @@ sharedSecretFilter <- function(req, res){
     supplied <- req$HTTP_PLUMBER_SHARED_SECRET
     if (!identical(supplied, secret)){
       res$status <- 400
+      # Force the route to return as unboxed json
+      res$serializer <- serializer_unboxed_json()
       return(list(error = "Shared secret mismatch."))
     }
   }
