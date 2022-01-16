d6d58659d1b9e26bb7d0a3cf421d72dfa5d72172
Mauricio Vargas <mavargas11@uc.cl>
2022-01-15 19:29:43 -0500

    update tests for feather + add tests for parquet

--- tests/testthat/test-serializer-parquet.R
+++ tests/testthat/test-serializer-parquet.R
@@ -1,16 +1,16 @@
-context("feather serializer")
+context("parquet serializer")
 
-test_that("feather serializes properly", {
+test_that("parquet serializes properly", {
   skip_if_not_installed("arrow")
 
   d <- data.frame(a=1, b=2, c="hi")
-  val <- serializer_feather()(d, data.frame(), PlumberResponse$new(), stop)
+  val <- serializer_parquet()(d, data.frame(), PlumberResponse$new(), stop)
   expect_equal(val$status, 200L)
-  expect_equal(val$headers$`Content-Type`, "application/feather")
+  expect_equal(val$headers$`Content-Type`, "application/parquet")
 
   # can test  by doing a full round trip if we believe the parser works via `test-parse-body.R`
-  parsed <- parse_body(val$body, "application/feather", make_parser("feather"))
-  # convert from feather tibble to data.frame
+  parsed <- parse_body(val$body, "application/parquet", make_parser("parquet"))
+  # convert from parquet tibble to data.frame
   parsed <- as.data.frame(parsed, stringsAsFactors = FALSE)
   attr(parsed, "spec") <- NULL
 
@@ -26,14 +26,14 @@ test_that("Errors call error handler", {
   }
 
   expect_equal(errors, 0)
-  serializer_feather()(parse(text="hi"), data.frame(), PlumberResponse$new("csv"), errorHandler = errHandler)
+  serializer_parquet()(parse(text="hi"), data.frame(), PlumberResponse$new("csv"), errorHandler = errHandler)
   expect_equal(errors, 1)
 })
 
 test_that("Errors are rendered correctly with debug TRUE", {
   skip_if_not_installed("arrow")
 
-  pr <- pr() %>% pr_get("/", function() stop("myerror"), serializer = serializer_feather()) %>% pr_set_debug(TRUE)
+  pr <- pr() %>% pr_get("/", function() stop("myerror"), serializer = serializer_parquet()) %>% pr_set_debug(TRUE)
   capture.output(res <- pr$serve(make_req(pr = pr), PlumberResponse$new("csv")))
 
   expect_match(res$body, "Error in (function () : myerror", fixed = TRUE)
