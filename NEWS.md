59333b4e1c4b64dfe7af90e58fcf8a7653843557
Barret Schloerke <schloerke@gmail.com>
2022-06-02 15:08:48 -0400

    Add `{coro}` into the future example api (#785)

--- NEWS.md
+++ NEWS.md
@@ -7,6 +7,7 @@
 * Introduces new GeoJSON serializer and parser. GeoJSON objects are parsed into `sf` objects and `sf` or `sfc` objects will be serialized into GeoJSON. (@josiahparry, #830)
 * Update feather serializer to use the arrow package. The new default feather MIME type is `application/vnd.apache.arrow.file`. (@pachadotdev #849)
 * Add parquet serializer and parser by using the arrow package (@pachadotdev #849)
+* Updated example `14-future` to use `promises::future_promise()` and added an endpoint that uses `{coro}` to write _simpler_ async / `{promises}` code (#785)
 
 ## Bug fixes
 
--- inst/plumber/14-future/plumber.R
+++ inst/plumber/14-future/plumber.R
@@ -1,9 +1,9 @@
 
 library(promises)
-library(future)
-
+library(coro)
 future::plan("multisession") # a worker for each core
 # future::plan(future::multisession(workers = 2)) # only two workers
+
 
 # Quick manual test:
 # Within 10 seconds...
@@ -24,14 +24,51 @@ function() {
 #' @get /future
 function() {
 
-  future({
-    # perform large computations
+  future_promise({
+    # perform long running computations
     Sys.sleep(10)
 
     # print route, time, and worker pid
     paste0("/future; ", Sys.time(), "; pid:", Sys.getpid())
   })
 }
+
+
+# A function that will return a promise object
+calc_using_promise <- function() {
+  # Executes using `future`
+  future_promise({
+    # perform long computations
+    Sys.sleep(1)
+
+    # return which process id was used
+    Sys.getpid()
+  }) %...>% {
+    exec_pid <- .
+    main_pid <- Sys.getpid()
+    # print route, time, and worker pid
+    paste0("/coro; ", Sys.time(), "; exec pid:", exec_pid, "; main pid:", main_pid)
+
+    list(exec_pid = exec_pid, main_pid = main_pid)
+  }
+}
+
+#' For more info about `coro`, visit https://coro.r-lib.org
+#' @contentType text
+#' @serializer json list(auto_unbox = TRUE)
+#' @get /coro
+async(function() {
+
+  # (a)wait for the promised value and use as a regular value
+  # No neeed for a followup promise when using `await(p)`
+  pids <- await(calc_using_promise())
+
+  # Write code with the finalized async value as if it was synchronously found
+  pids$exec_is_even <- pids$exec_pid %% 2 == 0
+
+  # return info
+  pids
+})
 
 
 # -----------------------------------
@@ -43,7 +80,7 @@ function() {
 #' @param a number
 #' @param b number
 function(a = NA, b = NA) {
-  future({
+  future_promise({
     a <- as.numeric(a)
     b <- as.numeric(b)
     if (is.na(a)) stop("a is missing")
@@ -59,7 +96,7 @@ function(a = NA, b = NA) {
 #' @param a number
 #' @param b number
 function(a = NA, b = NA) {
-  future({
+  future_promise({
     a <- as.numeric(a)
     b <- as.numeric(b)
     if (is.na(a)) stop("a is missing")
@@ -67,9 +104,10 @@ function(a = NA, b = NA) {
     if (b == 0) stop("Cannot divide by 0")
 
     a / b
-  }) %>%
+  }) %...!%
     # Handle `future` errors
-    promises::catch(function(error) {
+    {
+      error <- .
       # handle error here!
       if (error$message == "b is missing") {
         return(Inf)
@@ -77,5 +115,5 @@ function(a = NA, b = NA) {
 
       # rethrow original error
       stop(error)
-    })
+    }
 }
