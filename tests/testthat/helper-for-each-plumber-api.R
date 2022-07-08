b3473e2b819ba00afab7c05b58a67d8597815ac7
Barret Schloerke <schloerke@gmail.com>
2022-07-08 01:31:14 -0400

    Update with comments from Henrik

--- tests/testthat/helper-for-each-plumber-api.R
+++ tests/testthat/helper-for-each-plumber-api.R
@@ -4,8 +4,12 @@ for_each_plumber_api <- function(fn, ...) {
     available_apis(package)$name,
     function(name) {
       if (name == "14-future") {
-        if (isWindows()) {
-          message("Skipping test on 14-future example on Windows")
+        if (.Platform$OS.type == "windows" && getRversion() >= "4.0.0") {
+          # Future is not cleaned up properly and produces detritus temp files
+          # Debug PR: https://github.com/rstudio/plumber/pull/870
+          # Explain: https://github.com/HenrikBengtsson/future/blame/7a15808391b604903659bf67f9aa809f4c9a54c4/cran-comments.md
+          # Work around: https://github.com/HenrikBengtsson/future/blob/12cf573dc60cad82a22f03ff6e144201ba1bd42c/tests/nested_futures%2Cmc.cores.R#L5-L15
+          message("Skipping test on 14-future example on Windows R >= 4.0")
           return()
         }
         if (!require("future", character.only = TRUE, quietly = TRUE)) {
