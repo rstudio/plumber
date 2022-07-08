9b6694937f172b676416b977d1faf3fbc0dd2ad6
Barret Schloerke <schloerke@gmail.com>
2022-07-08 01:49:13 -0400

    Update helper-for-each-plumber-api.R

--- tests/testthat/helper-for-each-plumber-api.R
+++ tests/testthat/helper-for-each-plumber-api.R
@@ -9,6 +9,7 @@ for_each_plumber_api <- function(fn, ...) {
           # Debug PR: https://github.com/rstudio/plumber/pull/870
           # Explain: https://github.com/HenrikBengtsson/future/blame/7a15808391b604903659bf67f9aa809f4c9a54c4/cran-comments.md
           # Work around: https://github.com/HenrikBengtsson/future/blob/12cf573dc60cad82a22f03ff6e144201ba1bd42c/tests/nested_futures%2Cmc.cores.R#L5-L15
+          # R devel post: https://stat.ethz.ch/pipermail/r-devel/2021-June/080830.html
           message("Skipping test on 14-future example on Windows R >= 4.0")
           return()
         }
