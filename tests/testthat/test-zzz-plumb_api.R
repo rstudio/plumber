dad6880d429cd391a0b3a659f23ee366b682d919
Barret Schloerke <schloerke@gmail.com>
2022-07-08 00:57:00 -0400

    plumb apis - 3rd set

--- tests/testthat/test-zzz-plumb_api.R
+++ tests/testthat/test-zzz-plumb_api.R
@@ -16,72 +16,72 @@ expect_apis <- function(apis) {
 #   apis <- available_apis("plumber")
 #   expect_apis(apis)
 # })
-test_that("available_apis() print method works", {
-  apis_output <- capture.output({
-    available_apis("plumber")
-  })
+# test_that("available_apis() print method works", {
+#   apis_output <- capture.output({
+#     available_apis("plumber")
+#   })
 
-  expected_apis_output <- c(
-    "Available Plumber APIs:",
-    "* plumber",
-    paste0("  - ", dir(system.file("plumber", package = "plumber")))
-  )
+#   expected_apis_output <- c(
+#     "Available Plumber APIs:",
+#     "* plumber",
+#     paste0("  - ", dir(system.file("plumber", package = "plumber")))
+#   )
 
-  expect_equal(
-    apis_output,
-    expected_apis_output
-  )
-})
-test_that("available_apis() print method works with two packages", {
-  top <- available_apis("plumber")
-  bottom <- top
-  top$package <- "top"
-  bottom$package <- "bottom"
+#   expect_equal(
+#     apis_output,
+#     expected_apis_output
+#   )
+# })
+# test_that("available_apis() print method works with two packages", {
+#   top <- available_apis("plumber")
+#   bottom <- top
+#   top$package <- "top"
+#   bottom$package <- "bottom"
 
-  apis_output <- capture.output({
-    rbind(top, bottom)
-  })
+#   apis_output <- capture.output({
+#     rbind(top, bottom)
+#   })
 
-  plumber_apis <- paste0("  - ", dir(system.file("plumber", package = "plumber")))
+#   plumber_apis <- paste0("  - ", dir(system.file("plumber", package = "plumber")))
 
-  # printed in alpha order
-  expected_apis_output <- c(
-    "Available Plumber APIs:",
-    "* bottom",
-    plumber_apis,
-    "* top",
-    plumber_apis
-  )
+#   # printed in alpha order
+#   expected_apis_output <- c(
+#     "Available Plumber APIs:",
+#     "* bottom",
+#     plumber_apis,
+#     "* top",
+#     plumber_apis
+#   )
 
-  expect_equal(
-    apis_output,
-    expected_apis_output
-  )
-})
-
-# test_that("missing args are handled", {
-#   expect_equal(plumb_api("plumber", NULL), available_apis("plumber"))
-
-#   skip_on_cran()
-#   all_apis <- available_apis()
-#   expect_equal(plumb_api(NULL, "01-append"), all_apis)
-#   expect_equal(plumb_api(NULL, NULL), all_apis)
+#   expect_equal(
+#     apis_output,
+#     expected_apis_output
+#   )
 # })
 
-# test_that("errors are thrown", {
+test_that("missing args are handled", {
+  expect_equal(plumb_api("plumber", NULL), available_apis("plumber"))
+
+  skip_on_cran()
+  all_apis <- available_apis()
+  expect_equal(plumb_api(NULL, "01-append"), all_apis)
+  expect_equal(plumb_api(NULL, NULL), all_apis)
+})
+
+test_that("errors are thrown", {
 
 
-#   expect_error(plumb_api(c("plumber", "plumber"), "01-append"))
-#   expect_error(plumb_api("plumber", c("01-append", "01-append")))
+  expect_error(plumb_api(c("plumber", "plumber"), "01-append"))
+  expect_error(plumb_api("plumber", c("01-append", "01-append")))
 
-#   expect_error(plumb_api(TRUE, "01-append"))
-#   expect_error(plumb_api("plumber", TRUE))
+  expect_error(plumb_api(TRUE, "01-append"))
+  expect_error(plumb_api("plumber", TRUE))
 
-#   expect_error(plumb_api("plumber", "not an api"))
+  expect_error(plumb_api("plumber", "not an api"))
 
-#   expect_error(available_apis("not a package"), "No package found with name")
-#   expect_error(available_apis("crayon"), "No Plumber APIs found for package")
-# })
+  expect_error(available_apis("not a package"), "No package found with name")
+  expect_error(available_apis("crayon"), "No Plumber APIs found for package")
+})
 
 # test_that("edit opens correct file", {
 #   # Redefine editor so that file.edit doesn't try to open a file
