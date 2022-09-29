3f1cc4ce39e0f0822056146b2c4c981b023544c7
Barret Schloerke <schloerke@gmail.com>
2022-09-29 09:28:30 -0400

    Test mount falling back to parent mount

--- tests/testthat/files/router.R
+++ tests/testthat/files/router.R
@@ -61,3 +61,17 @@ function(res){
 function(){
   "dual path"
 }
+
+#* @plumber
+function(pr) {
+  mnt_1 <-
+    pr() %>%
+    pr_get("/hello", function() "say hello")
+  mnt_2 <-
+    pr() %>%
+    pr_get("/world", function() "say hello world")
+
+  pr %>%
+    pr_mount("/say", mnt_1) %>%
+    pr_mount("/say/hello", mnt_2)
+}
